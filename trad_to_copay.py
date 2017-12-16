2faa5c2228e272aae8dd5f9e2eaa888999627b15
kakashi-1 <34309321+kakashi-1@users.noreply.github.com>
2017-12-15 21:51:41 -0500

    Update and rename trad_to_copay.py to trad_to_bitpay.py

--- trad_to_copay.py
+++
@@ -1,9 +0,0 @@
-import base58
-
-# translate traditional address to Copay's address format
-trad_address = '19oCM37SX7R7ctVPwv1pQ2RckTM3TmU9dS'
-public_key_hash = base58.decodeBase58Check(trad_address,0)
-copay_address = base58.encodeBase58Check(public_key_hash,28)
-print "Copay Address: ", copay_address
-
-
